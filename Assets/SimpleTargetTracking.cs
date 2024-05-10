0988f833343709d606aff8e84235d4ead716a617
PatriciaRdrz <patricia.r@opendeusto.es>
2024-05-10 09:58:16 +0200

    Navegation y obstáculos

--- Assets/SimpleTargetTracking.cs
+++ Assets/SimpleTargetTracking.cs
@@ -6,16 +6,24 @@ using UnityEngine;
 public class SimpleTargetTracking : MonoBehaviour
 {
     // Start is called before the first frame update
-    public Transform goal;
-    private NavMeshAgent agent; 
+    public List<Transform> goals;
+    private NavMeshAgent agent;
+    int i;
     void Start()
     {
-        agent = GetComponent<>
+        agent = GetComponent<NavMeshAgent>();
+        i = 0;
+        agent.SetDestination(goals[i].position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (agent.remainingDistance <= agent.stoppingDistance)
+        {
+            agent.SetDestination(goals[i].position);
+            i++;
+            if (i == goals.Count) i = 0;
+        }
     }
 }
