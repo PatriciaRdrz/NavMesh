1c54c6f9cc6e4e8197ffb160dd8c8a4f07b6bd3b
PatriciaRdrz <patricia.r@opendeusto.es>
2024-05-10 08:47:05 +0200

    Primer commit

---
+++ Assets/SimpleTargetTracking.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.AI;
+using UnityEngine;
+
+public class SimpleTargetTracking : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Transform goal;
+    private NavMeshAgent agent; 
+    void Start()
+    {
+        agent = GetComponent<>
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
